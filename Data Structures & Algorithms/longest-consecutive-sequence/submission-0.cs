d4f2dc1cb02611482c534cf1f15e48c8bb9d9c04
RampantBias <75921791+RampantBias@users.noreply.github.com>
2026-08-21 13:59:19 -0500

    Add: longest-consecutive-sequence - submission-0

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int LongestConsecutive(int[] nums) {
+        if (nums.Length <= 1) {
+            return nums.Length;
+        }
+
+        int maxLength = 1;
+        HashSet<int> hashed = new HashSet<int>(nums);
+        foreach(int num in nums) {
+            if (hashed.Contains(num - 1)) {
+                continue;
+            } else {
+                int length = 1;
+                int next = num + 1;
+                while (hashed.Contains(next)) {
+                    length++;
+                    next++;
+                    maxLength = Math.Max(maxLength, length);
+                }
+            }
+        }
+        return maxLength;
+    }
+}
