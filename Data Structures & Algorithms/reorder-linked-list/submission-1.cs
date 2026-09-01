8af814261307094707a0ea85ca7cfff37c5ac11a
RampantBias <75921791+RampantBias@users.noreply.github.com>
2026-09-01 17:31:06 -0500

    Add: reorder-linked-list - submission-1

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-1.cs
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public void ReorderList(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode current = slow.next;
+        slow.next = null; 
+        ListNode previous = null;
+        while (current != null) {
+            ListNode nextTemp = current.next;
+            current.next = previous;
+            previous = current;
+            current = nextTemp;
+        }
+
+        ListNode first = head;
+        ListNode second = previous;
+        while (second != null) {
+            ListNode firstNext = first.next;
+            ListNode secondNext = second.next;
+
+            first.next = second;
+            second.next = firstNext;
+
+            first = firstNext;
+            second = secondNext;
+        }
+    }
+}
